92f21a8d010661ea1b2afef49685bb972f30136e
Ding Li <1995dingli@gmail.com>
2020-01-09 00:23:22 +0800

    rename start

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+cd $SHELL_FOLDER
+pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+gunicorn --bind 0.0.0.0:80 --workers=2 'chainmore:create_app()'
